2bed524a6fb99ef61ce2e5d835af22be8d05b94a
unknown <ricardo.andrade@ucb.edu.bo>
2026-04-28 13:48:51 -0400

    feat: implementacion inicial de estructura y preparacion de entorno de trabajo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import '@/styles/main.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="app-container">
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
