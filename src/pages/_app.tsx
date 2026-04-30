c105c32638a4687172f5947982677da05487fb39
FullMaury <mauricitogarron@gmail.com>
2026-04-30 09:16:29 -0400

    feat: agregar navbar global a la aplicacion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
 import '@/styles/main.scss';
+import Navbar from '@/components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="app-container">
+      <Navbar />
       <main className="container">
         <Component {...pageProps} />
       </main>
