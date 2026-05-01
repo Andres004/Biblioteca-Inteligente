ab94c24f6219f3662327479a1c015ca51292d37a
unknown <ricardo.andrade@ucb.edu.bo>
2026-05-01 06:40:05 -0400

    feat: conecta inicio con api y muestra libros

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,46 @@
+import { useEffect, useState } from 'react';
+import { Navbar } from '../components/NavBar';
+import { BookCard } from '../components/BookCard';
+import { Loading } from '../components/Loading';
+import { searchBooks } from '../services/openLibraryService';
+
 export default function Home() {
-  return <h1>Hola, Biblioteca Inteligente</h1>;
+  const [books, setBooks] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchInicio = async () => {
+      const res = await searchBooks('programming');
+      setBooks(res);
+      setLoading(false);
+    };
+    fetchInicio();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h2 style={{ paddingLeft: '20px' }}>Libros Destacados</h2>
+      
+      {loading ? (
+        <Loading />
+      ) : (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {books.map((b: any) => (
+            <BookCard
+              key={b.id}
+              id={b.id}
+              title={b.title}
+              author={b.author}
+              firstPublishYear={b.year}
+              editionCount={b.editions}
+              isFavorite={false}
+              onToggleFavorite={(id) => console.log('agregar a fav', id)}
+              onViewDetails={(id) => window.location.href = `/libro/${id}`}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
